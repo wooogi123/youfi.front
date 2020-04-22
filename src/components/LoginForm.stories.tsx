8e5620dd4c5954c2b1678b74dc50da17f0727f6b
akalee <wooogi123@gmail.com>
2020-04-22 21:13:56 +0900

    storybook attached

---
+++ src/components/LoginForm.stories.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import LoginForm from './LoginForm';
+
+export default {
+  title: 'components/LoginForm',
+  component: LoginForm,
+};
+
+export const standard = () => <LoginForm />;
