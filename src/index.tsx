5890006cbfcfd62f3818d8b44d761d999657e0f3
akalee <wooogi123@gmail.com>
2020-05-27 13:01:56 +0900

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,23 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createStore, applyMiddleware, compose } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
 import createSagaMiddleware from 'redux-saga';
 import rootReducer, { rootSaga, dictionaryAction } from './store';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-declare global {
-  interface Window {
-    __REDUX_DEVTOOLS_EXTENSION_COMPOSE__?: typeof compose;
-  }
-}
-
 const sagaMiddleware = createSagaMiddleware();
-const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-const store = createStore(rootReducer, /* preloadedState, */ composeEnhancer(
-  applyMiddleware(sagaMiddleware),
-));
+const store = createStore(rootReducer, applyMiddleware(sagaMiddleware));
 sagaMiddleware.run(rootSaga);
 
 store.dispatch(dictionaryAction.request('/dictionary.json'));
