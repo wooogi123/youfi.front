10299a307a1599b1b958162f80f25d080f85f7bc
Akalee <wooogi123@gmail.com>
2020-05-29 19:50:04 +0900

    bankName search bar add

--- src/containers/DepositContainer.tsx
+++ src/containers/DepositContainer.tsx
@@ -1,14 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useDepositStore } from '../hooks';
 import DepositService from '../components/service/DepositService';
 
 function DepositContainer() {
+  const [search, setSearch] = useState('');
   const store = useDepositStore();
+
+  function onChange(e?: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) {
+    if (e !== undefined) setSearch(e.currentTarget.value);
+  }
 
   return (
     <DepositService
-      contents={store.contents.results}
-      isSearch={false}
+      contents={store.contents.results.filter((el) => (el.base.kor_co_nm.indexOf(search) !== -1))}
+      isSearch
+      search={search}
+      onChangeSearch={onChange}
     />
   );
 }
