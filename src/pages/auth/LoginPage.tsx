c27eada793d642e33075587c174af39444910fce
Akalee <wooogi123@gmail.com>
2020-07-01 21:19:42 +0900

    del containers

--- src/pages/auth/LoginPage.tsx
+++ src/pages/auth/LoginPage.tsx
@@ -1,9 +1,41 @@
-import React from 'react';
-import LoginContainer from '../../containers/LoginContainer';
+import React, { useState } from 'react';
+import { useAuthStore, useLoginAction } from '../../hooks';
+import AuthForm from '../../components/auth/AuthForm';
 
 function LoginPage() {
+  const store = useAuthStore();
+  const [login, setLogin] = useState({
+    email: '',
+    password: '',
+  });
+
+  const loginAction = useLoginAction();
+
+  function onChange(e?: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) {
+    setLogin({
+      ...login,
+      [e?.currentTarget.name as string]: e?.currentTarget.value as string,
+    });
+  }
+
+  function onClick(e?: React.MouseEvent<HTMLButtonElement>) {
+    e?.preventDefault();
+    loginAction(login);
+    setLogin({
+      ...login,
+      password: '',
+    });
+  }
+
   return (
-    <LoginContainer />
+    <AuthForm
+      type={'login'}
+      email={login.email}
+      password={login.password}
+      isError={store.isError.login}
+      onChange={onChange}
+      onClick={onClick}
+    />
   );
 }
 
--- src/pages/auth/RegisterPage.tsx
+++ src/pages/auth/RegisterPage.tsx
@@ -1,9 +1,44 @@
-import React from 'react';
-import RegisterContainer from '../../containers/RegisterContainer';
+import React, { useState } from 'react';
+import { useAuthStore, useRegisterAction } from '../../hooks';
+import AuthForm from '../../components/auth/AuthForm';
 
 function RegisterPage() {
+  const store = useAuthStore();
+  const [register, setRegister] = useState({
+    email: '',
+    password: '',
+    passwordConfirm: '',
+  });
+
+  const registerAction = useRegisterAction();
+
+  function onChange(e?: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) {
+    setRegister({
+      ...register,
+      [e?.currentTarget.name as string]: e?.currentTarget.value as string,
+    });
+  }
+
+  function onClick(e?: React.MouseEvent<HTMLButtonElement>) {
+    e?.preventDefault();
+    registerAction(register);
+    setRegister({
+      email: '',
+      password: '',
+      passwordConfirm: '',
+    });
+  }
+
   return (
-    <RegisterContainer />
+    <AuthForm
+      type={'register'}
+      email={register.email}
+      password={register.password}
+      passwordConfirm={register.passwordConfirm}
+      isError={store.isError.register}
+      onChange={onChange}
+      onClick={onClick}
+    />
   );
 }
 
